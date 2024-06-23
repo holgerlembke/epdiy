2169ec59b36ac56ba2c365b53d3d761d065f9606
Holger Lembke <lembke@gmail.com>
2024-06-23 19:32:21 +0200

    Additional examples V2

---
+++ examples/Basics/A_First_Steps/A_First_Steps.ino
@@ -0,0 +1,58 @@
+/*
+  Moin.
+
+  This is the first step in using epdiy (https://github.com/vroland/epdiy) within the Arduino IDE.
+  It is my idea how Arduino programs should by structured. You might have a different idea.
+
+  Goal:
+    - get the hardware configured
+    - include epdiy
+    - have a tab with all the stuff to get the display running (easy copy-paste-solution)
+    - paint some lines and a circle 
+
+  Hardware:
+    This guide assumes the use of the "Epdiy V7" board with a ESP32-S3-Wroom-N16R8 
+    see https://vroland.github.io/epdiy-hardware/
+
+  Arduino-Board-Settings:
+     Board: ESP32S3 Dev Modul
+     Flash Size: 16 MB 
+     Part Scheme: 16 MB Flash (2 MB App, 12,5 MB FFATFS) 
+               or 16 MB Flash (3 MB App, 9.6 MB FFATFS) 
+     PSRAM: OPI PSRAM
+
+     (everything else is default for this modul)
+
+
+  Compile order for tabs:
+     The Arduino IDE compiles the sketches/tab like this:
+       - all tab content is added together
+         -- first the main tab (always on the left side.)
+         -- the other tabs in alphabetically ASCII order
+            (so ABC...abc!)
+       - while doing that, all function calls are collected and inserted 
+         after the last #include in the main tab
+     This gives a .cpp file that is passed to the c++ compiler.      
+
+*/
+
+#include <epdiy.h>
+
+//-----------------------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) { delay(100); }
+  Serial.print(F("\n\epdiy First"));
+  Serial.print(F(__DATE__));
+  Serial.print(" ");
+  Serial.print(F(__TIME__));
+  Serial.println();
+
+  setupBoard();
+  test();
+}
+
+//-----------------------------------------------------------------------------------------------
+void loop() {}
+
+//
--- examples/Basics/A_First_Steps/Aepdiy.ino
+++ examples/Basics/A_First_Steps/Aepdiy.ino
@@ -15,12 +15,25 @@ struct epdiydata_t {
 //*********************************************************************************************************************
 void setupBoard() {
   epd_init(&epd_board_v7,  // board with esp32-s3
-           &ED097TC2,      // epaper: ED097TC2
+           /*
+             Here goes the epaper board name as defined in src/displays.c 
+             You should find your board in this list: https://github.com/vroland/epdiy?tab=readme-ov-file#displays
+           */
+           &ED097TC2,  // epaper: ED097TC2
+           /*
+             Lookup-Table-Type for the Waveform. Tradeoff between memory usage and speed. 
+             Best idea: don't touch it. Play with it after every thing else works.
+           */
            EPD_LUT_64K);
-  epd_set_vcom(1560);
+  /*
+    This sets the display common voltage.
+    Usually it is a negative voltage somewhere documented. My ED097TC2 has a sticker on the board reading "-1.59"
+
+    The function parameter is given as a positive value in Millivolts.
+  */
+  epd_set_vcom(1590);
 
   epdiydata.hl = epd_hl_init(EPD_BUILTIN_WAVEFORM);
-  //epd_set_rotation(EPD_ROT_LANDSCAPE);
 
   epdiydata.epaperFrameBuffer = epd_hl_get_framebuffer(&epdiydata.hl);
   epdiydata.epaperfont_props = epd_font_properties_default();
@@ -39,14 +52,29 @@ void setupBoard() {
 }
 
 //*********************************************************************************************************************
+void checkError(enum EpdDrawError err) {
+  if (err != EPD_DRAW_SUCCESS) {
+    Serial.println("Panic: epdiy error " + String(err));
+  }
+}
+
+//*********************************************************************************************************************
 void epaperUpdateDisplay() {
   epd_poweron();
   int temperature = epd_ambient_temperature();
-  EpdDrawError err = epd_hl_update_screen(&epdiydata.hl, MODE_GC16, temperature);
+
+  //MODE_GC16 hmmmm streifen
+  //MODE_GL16 besser, aber löchrig
+  // MODE_DU
+
+  /* 
+     EpdDrawError err gives an error result. You might want to check it with checkError();
+     EPD_DRAW_SUCCESS is good, everything else not so.
+  */
+  EpdDrawError err = epd_hl_update_screen(&epdiydata.hl, MODE_DU, temperature);
+
   epd_poweroff();
 }
-
-
 
 
 
